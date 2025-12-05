bfe8443e49542a992273cb370a573d27cede37cd
Corentin Garcia <corenting@gmail.com>
2025-12-05 20:16:52 +0100

    2025 day 3 part 1

---
+++ 2025/day_3/part_1.py
@@ -0,0 +1,33 @@
+with open('input', 'r') as input_file:
+    lines = [i.strip() for i in input_file.readlines()]
+
+total_output = 0
+digits_required = 2
+
+bank_length = len(lines[0])
+
+for bank in lines:
+    nums = [int(battery) for battery in bank]
+    my_digits = []
+
+    digits_to_ignore = digits_required - 1
+    current_count = 0
+    prev_idx = 0
+    while current_count < digits_required:
+
+        if current_count == 0:
+            nums_slice = nums[:-1]
+        else:
+            nums_slice = nums[prev_idx:]
+
+        max_value = max(nums_slice)
+        max_index = nums.index(max_value)
+        nums[max_index] = -10000
+        my_digits.append(max_value)
+        current_count += 1
+        prev_idx = max_index
+
+
+    total_output += int("".join(str(i) for i in my_digits))
+
+print(f"Step 1: {total_output}")
